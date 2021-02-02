7b80b3bda69436683d1ec42830b115db6522f53a
AlexeyAbramov <abramov.alexey.93@ya.ru>
2021-02-02 16:06:08 +0300

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  $lang_menu = $(".menu-language");
+  $footer_menu = $(".footer-language");
+  $(".header-language__popup").click(function () {
+    // $('#menu-language').toggle();
+    $lang_menu.toggleClass("active");
+    $(".header-language__popup").toggleClass("active");
+  });
+  $(".footer-language__popup").click(function () {
+    $(this).toggleClass("active");
+    $(".footer-language__menu").toggleClass("active");
+  });
+  $('.header-menu').click(function(){
+    $(this).toggleClass('active');
+    $('.header-nav').toggleClass('active');
+  })
+
+
+  //slick
+
+  $(document).ready(function () {
+    $(".partners__list").slick({
+      slidesToShow: 4,
+      infinity: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      accessibility: false,
+      dots: true,
+      pauseOnFocus: false,
+      responsive:[
+        {
+          breakpoint: 760,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 860,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
+        },
+      ]
+    });
+  });
+  $(".subscribe__list").slick({
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 2900,
+        settings: "unslick",
+      },
+    ],
+  });
+});
