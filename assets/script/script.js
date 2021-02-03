c703bd0fd7cdc9e9da62b2b84b56fd75a26ce31c
AlexeyAbramov <abramov.alexey.93@ya.ru>
2021-02-03 14:13:17 +0300

    added animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,55 +1,204 @@
 $(document).ready(function () {
-  setInterval(() => {
-    $(".phone-header__mark").each(function (index) {
-      switch (index) {
-        case 0:
-          $(this).animate(
-            {
-              // opacity: "0",
-              right: `${getRandomArbitrary(35, 53)}%`,
-              top: `${getRandomArbitrary(4, 21)}%`,
-            },
-            0,
-            "linear"
-          );
-          break;
-        case 1:
-          $(this).animate(
-            {
-              // opacity: "0",
-              right: `${getRandomArbitrary(28, 42)}%`,
-              top: `${getRandomArbitrary(46, 49)}%`,
-            },
-            0,
-            "linear"
-          );
-          break;
-        case 2:
-          $(this).animate(
-            {
-              // opacity: "0",
-              right: `${getRandomArbitrary(1, 26)}%`,
-              top: `${getRandomArbitrary(3, 56)}%`,
-            },
-            0,
-            "linear"
-          );
-          break;
-        case 3:
-          $(this).animate(
-            {
-              // opacity: "0",
-              right: `${getRandomArbitrary(1, 36)}%`,
-              top: `${getRandomArbitrary(1, 34)}%`,
-            },
-            0,
-            "linear"
-          );
-        default:
-          break;
-      }
-    });
-  }, 3000);      // время анимации в мс (3000)
+  $icons = $(".phone-header__mark");
+  $icons__quantity = $icons.length;
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  if ($(window).width() > 860) {
+    setInterval(() => {
+      $icons.each(function (index) {
+        // возвращаем новую ссылку иконки
+        let randomSrc = (quantity) => {
+          let num = Math.floor(getRandomArbitrary(1, quantity));
+          return $(this)
+            .attr("src")
+            .replace(/([0-9]).png/, `${num}.png`);
+        };
+
+        $(this).attr("src", randomSrc($icons__quantity));
+
+        switch (index) {
+          case 0:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(35, 53)}%`,
+                top: `${getRandomArbitrary(4, 21)}%`,
+              },
+              0,
+              "linear"
+            );
+            lel = () => {
+              console.log("hi");
+            };
+            break;
+          case 1:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(28, 42)}%`,
+                top: `${getRandomArbitrary(46, 49)}%`,
+              },
+              0,
+              "linear"
+            );
+            break;
+          case 2:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(1, 26)}%`,
+                top: `${getRandomArbitrary(33, 56)}%`,
+              },
+              0,
+              "linear"
+            );
+            break;
+          case 3:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(1, 36)}%`,
+                top: `${getRandomArbitrary(1, 34)}%`,
+              },
+              0,
+              "linear"
+            );
+          default:
+            break;
+        }
+      });
+    }, 3000); // время анимации в мс (3000)
+  } else {
+    setInterval(() => {
+      $icons.each(function (index) {
+        // возвращаем новую ссылку иконки
+        let randomSrc = (quantity) => {
+          let num = Math.floor(getRandomArbitrary(1, quantity));
+          return $(this)
+            .attr("src")
+            .replace(/([0-9]).png/, `${num}.png`);
+        };
+
+        $(this).attr("src", randomSrc($icons__quantity));
+        
+        switch (index) {
+          case 0:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(35, 71)}%`,
+                top: `${getRandomArbitrary(5, 25)}%`,
+              },
+              0,
+              "linear"
+            );
+            lel = () => {
+              console.log("hi");
+            };
+            break;
+          case 1:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(33, 60)}%`,
+                top: `${getRandomArbitrary(20, 34)}%`,
+              },
+              0,
+              "linear"
+            );
+            break;
+          case 2:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(1, 35)}%`,
+                top: `${getRandomArbitrary(6, 25)}%`,
+              },
+              0,
+              "linear"
+            );
+            break;
+          case 3:
+            $(this).animate(
+              {
+                // opacity: "0",
+                right: `${getRandomArbitrary(5, 32)}%`,
+                top: `${getRandomArbitrary(20, 34)}%`,
+              },
+              0,
+              "linear"
+            );
+          default:
+            break;
+        }
+      });
+    }, 3000); // время анимации в мс (3000)
+  }
+
+  // setInterval(() => {
+  //   $icons.each(function (index) {
+  //     let randomSrc = (quantity) => {
+  //       let num = Math.floor(getRandomArbitrary(1, quantity));
+  //       return $(this)
+  //         .attr("src")
+  //         .replace(/([0-9]).png/, `${num}.png`);
+  //     };
+
+  //     $(this).attr("src", randomSrc($icons__quantity));
+  //     switch (index) {
+  //       case 0:
+  //         $(this).animate(
+  //           {
+  //             // opacity: "0",
+  //             right: `${getRandomArbitrary(35, 53)}%`,
+  //             top: `${getRandomArbitrary(4, 21)}%`,
+  //           },
+  //           0,
+  //           "linear"
+  //         );
+  //         lel = () => {
+  //           console.log("hi");
+  //         };
+  //         break;
+  //       case 1:
+  //         $(this).animate(
+  //           {
+  //             // opacity: "0",
+  //             right: `${getRandomArbitrary(28, 42)}%`,
+  //             top: `${getRandomArbitrary(46, 49)}%`,
+  //           },
+  //           0,
+  //           "linear"
+  //         );
+  //         break;
+  //       case 2:
+  //         $(this).animate(
+  //           {
+  //             // opacity: "0",
+  //             right: `${getRandomArbitrary(1, 26)}%`,
+  //             top: `${getRandomArbitrary(33, 56)}%`,
+  //           },
+  //           0,
+  //           "linear"
+  //         );
+  //         break;
+  //       case 3:
+  //         $(this).animate(
+  //           {
+  //             // opacity: "0",
+  //             right: `${getRandomArbitrary(1, 36)}%`,
+  //             top: `${getRandomArbitrary(1, 34)}%`,
+  //           },
+  //           0,
+  //           "linear"
+  //         );
+  //       default:
+  //         break;
+  //     }
+  //   });
+  // }, 3000); // время анимации в мс (3000)
 
   function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
